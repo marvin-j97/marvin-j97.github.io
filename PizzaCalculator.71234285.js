4ca96dc0a9f4d2d55458b0d05a1e113ab6cba5cf
marvin-j97 <33938500+marvin-j97@users.noreply.github.com>
2022-12-24 17:35:19 +0000

    Deploying to gh-pages from @ marvin-j97/marvin-j97.github.io@ffb76fecc14f8e26ddc0d3dedb6efebbe70acb6f 🚀

---
+++ PizzaCalculator.71234285.js
@@ -0,0 +1,23 @@
+import{g as p,i as r,c as f,t as y,s as h,a as c,b as m,d as g,e as n,S as X}from"./chunks/web.fda141a9.js";const Z=y(`<div><div class="mb-1 text-sm font-semibold sec--text"></div><input type="text" readonly class="text-gray-500
+      cursor-not-allowed
+      appearance-none
+      border
+      rounded
+      w-full
+      py-2
+      px-3
+      leading-tight
+      focus:outline-none focus:shadow-outline
+    "></div>`);function _(e){return(()=>{const o=p(Z),i=o.firstChild,a=i.nextSibling;return r(i,()=>e.title),f(()=>a.value=e.value),o})()}const ee=y(`<div><div class="mb-2 text-sm font-semibold sec--text"></div><input type="number" class="
+      shadow
+      appearance-none
+      border
+      focus:border-blue-500
+      rounded
+      w-full
+      py-2
+      px-3
+      text-gray-700
+      leading-tight
+      focus:outline-none focus:shadow-outline
+    "></div>`);function v(e){return(()=>{const o=p(ee),i=o.firstChild,a=i.nextSibling;return r(i,()=>e.title),a.addEventListener("change",t=>e.onChange(Number(t.currentTarget.value)||0)),f(t=>{const x=e.step||1,d=e.min,$=e.max;return x!==t._v$&&h(a,"step",t._v$=x),d!==t._v$2&&h(a,"min",t._v$2=d),$!==t._v$3&&h(a,"max",t._v$3=$),t},{_v$:void 0,_v$2:void 0,_v$3:void 0}),f(()=>a.value=e.value),o})()}const te=y('<div class="flex flex-col gap-4"><!#><!/><hr class="border-0 bg-gray-300 text-gray-500 h-px"><!#><!/><!#><!/><!#><!/><!#><!/><label><input class="mr-2" id="advanced-settings-checkbox" type="checkbox">Erweitert</label><!#><!/></div>');function S(e){return e/100}function u(e){return e.toFixed(2)}function ae(){const[e,o]=c(1),[i,a]=c(140),[t,x]=c(S(65)),[d,$]=c(S(.5)),[z,T]=c(S(2.5)),[C,A]=c(!1),b=m(()=>e()*i()),F=m(()=>b()*t()),H=m(()=>b()*z()),L=m(()=>b()*d());return(()=>{const l=p(te),W=l.firstChild,[w,D]=g(W.nextSibling),I=w.nextSibling,Y=I.nextSibling,[N,j]=g(Y.nextSibling),q=N.nextSibling,[R,B]=g(q.nextSibling),G=R.nextSibling,[k,J]=g(G.nextSibling),K=k.nextSibling,[P,O]=g(K.nextSibling),E=P.nextSibling,M=E.firstChild,Q=E.nextSibling,[U,V]=g(Q.nextSibling);return r(l,n(v,{min:1,max:1e3,title:"Pizzen (ganz)",get value(){return e()},onChange:o}),w,D),r(l,n(_,{title:"Mehl (g)",get value(){return u(b())}}),N,j),r(l,n(_,{title:"Wasser (ml)",get value(){return u(F())}}),R,B),r(l,n(_,{title:"Trockenhefe (g)",get value(){return u(L())}}),k,J),r(l,n(_,{title:"Trockenhefe (g)",get value(){return u(H())}}),P,O),M.addEventListener("change",s=>A(s.currentTarget.checked)),r(l,n(X,{get when(){return C()},get children(){return[n(v,{min:100,max:200,title:"Mehl pro Pizza",get value(){return i()},onChange:a}),n(v,{min:50,max:80,title:"Hydration (%)",get value(){return t()*100},onChange:s=>x(Number(u(s/100))),step:.5}),n(v,{min:0,max:2,title:"Hefeanteil (%)",get value(){return d()*100},onChange:s=>$(Number(u(s/100))),step:.1}),n(v,{min:0,max:5,title:"Salzanteil (%)",get value(){return z()*100},onChange:s=>T(Number(u(s/100))),step:.1})]}}),U,V),f(()=>M.checked=C()),l})()}export{ae as default};
